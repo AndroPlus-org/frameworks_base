5aa948f63ec6d04ad424d72ea8c654e6ac7368d9
phhusson <phh@phh.me>
2019-04-05 16:44:18 +0200

    Initial support for OnePlus in-display fingerprint sensor
    Tested on OnePlus 6T.
    
    Here are the list of things that are device-specific:
    - Display modes
    - fingerprint control (tell the fingerprint sensor the green circle is
    ready, and what is its brightness)
    
    Omni : Adapted for OnePlus
    TODO: read circle position and size from overlays
    
    Original commit : https://github.com/phhusson/platform_frameworks_base/commit/64b2e6d1fc378ac13783f5002e5a9d9379e75f3b
    
    Change-Id: Ia4e0afeb3a1fa8c53d2838ece989aa9c5badd84f

--- services/core/java/com/android/server/fingerprint/AuthenticationClient.java
+++ services/core/java/com/android/server/fingerprint/AuthenticationClient.java
@@ -17,6 +17,8 @@
 package com.android.server.fingerprint;
 
 import android.content.Context;
+import android.content.ComponentName;
+import android.content.res.Resources;
 import android.hardware.biometrics.fingerprint.V2_1.IBiometricsFingerprint;
 import android.hardware.biometrics.BiometricPrompt;
 import android.hardware.biometrics.IBiometricPromptReceiver;
@@ -26,11 +28,14 @@ import android.hardware.fingerprint.IFingerprintServiceReceiver;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.content.pm.PackageManager;
 import android.util.Slog;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.IStatusBarService;
+
+import vendor.oneplus.fingerprint.extension.V1_0.IVendorFingerprintExtensions;
 
 /**
  * A class to keep track of the authentication state for a given client.
@@ -55,6 +60,11 @@ public abstract class AuthenticationClient extends ClientMonitor {
     protected boolean mDialogDismissed;
 
     private FacolaView mFacola;
+    private boolean mDisplayFODView;
+    private IVendorFingerprintExtensions mExtDaemon = null;
+    private final String mKeyguardPackage;
+    private static final int DISABLE_FP_LONGPRESS = 4;
+    private static final int ENABLE_FP_LONGPRESS = 3;
 
     // Receives events from SystemUI and handles them before forwarding them to FingerprintDialog
     protected IBiometricPromptReceiver mDialogReceiver = new IBiometricPromptReceiver.Stub() {
@@ -99,6 +109,9 @@ public abstract class AuthenticationClient extends ClientMonitor {
         mFingerprintManager = (FingerprintManager) getContext()
                 .getSystemService(Context.FINGERPRINT_SERVICE);
         mFacola = new FacolaView(context);
+        mDisplayFODView = context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+        mKeyguardPackage = ComponentName.unflattenFromString(context.getResources().getString(
+                com.android.internal.R.string.config_keyguardComponent)).getPackageName();
     }
 
     @Override
@@ -236,7 +249,13 @@ public abstract class AuthenticationClient extends ClientMonitor {
             resetFailedAttempts();
             onStop();
         }
-        if(result == true) mFacola.hide();
+        if(result == true && mDisplayFODView) {
+            try {
+                mStatusBarService.handleInDisplayFingerprintView(false, false);
+            } catch (RemoteException e) {}
+        } else if (result == true) {
+            mFacola.hide() 
+        }
         return result;
     }
 
@@ -250,7 +269,23 @@ public abstract class AuthenticationClient extends ClientMonitor {
             Slog.w(TAG, "start authentication: no fingerprint HAL!");
             return ERROR_ESRCH;
         }
-        mFacola.show();
+
+        if (mDisplayFODView) {
+            try {
+                mExtDaemon = IVendorFingerprintExtensions.getService();
+                Slog.w(TAG, "getOwnerString : " + isKeyguard(getOwnerString()));
+
+                if (isKeyguard(getOwnerString())) {
+                    mExtDaemon.updateStatus(ENABLE_FP_LONGPRESS);
+                } else {
+                    mExtDaemon.updateStatus(DISABLE_FP_LONGPRESS);
+                }
+
+                mStatusBarService.handleInDisplayFingerprintView(true, false);
+            } catch (RemoteException e) {}
+        } else {
+            mFacola.show();
+        }
         onStart();
         try {
             final int result = daemon.authenticate(mOpId, getGroupId());
@@ -263,7 +298,7 @@ public abstract class AuthenticationClient extends ClientMonitor {
             if (DEBUG) Slog.w(TAG, "client " + getOwnerString() + " is authenticating...");
 
             // If authenticating with system dialog, show the dialog
-            if (mBundle != null) {
+            if (!mDisplayFODView && mBundle != null) {
                 try {
                     mStatusBarService.showFingerprintDialog(mBundle, mDialogReceiver);
                 } catch (RemoteException e) {
@@ -277,6 +312,14 @@ public abstract class AuthenticationClient extends ClientMonitor {
         return 0; // success
     }
 
+    /**
+     * @param clientPackage
+     * @return true if this is keyguard package
+     */
+    private boolean isKeyguard(String clientPackage) {
+        return mKeyguardPackage.equals(clientPackage);
+    }
+
     @Override
     public int stop(boolean initiatedByClient) {
         if (mAlreadyCancelled) {
@@ -284,7 +327,14 @@ public abstract class AuthenticationClient extends ClientMonitor {
             return 0;
         }
 
-        mFacola.hide();
+        if (mDisplayFODView) {
+            try {
+                mStatusBarService.handleInDisplayFingerprintView(false, false);
+            } catch (RemoteException e) {}
+        } else {
+            mFacola.hide();
+        }
+
         onStop();
         IBiometricsFingerprint daemon = getFingerprintDaemon();
         if (daemon == null) {
@@ -306,7 +356,7 @@ public abstract class AuthenticationClient extends ClientMonitor {
             // dialog, we do not need to hide it since it's already hidden.
             // If the device is in lockout, don't hide the dialog - it will automatically hide
             // after BiometricPrompt.HIDE_DIALOG_DELAY
-            if (mBundle != null && !mDialogDismissed && !mInLockout) {
+            if (!mDisplayFODView && mBundle != null && !mDialogDismissed && !mInLockout) {
                 try {
                     mStatusBarService.hideFingerprintDialog();
                 } catch (RemoteException e) {
--- services/core/java/com/android/server/fingerprint/EnrollClient.java
+++ services/core/java/com/android/server/fingerprint/EnrollClient.java
@@ -17,6 +17,7 @@
 package com.android.server.fingerprint;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.hardware.biometrics.fingerprint.V2_1.IBiometricsFingerprint;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.IFingerprintServiceReceiver;
@@ -26,9 +27,11 @@ import android.util.Slog;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.statusbar.IStatusBarService;
 
 import java.util.Arrays;
 
+import vendor.oneplus.fingerprint.extension.V1_0.IVendorFingerprintExtensions;
 
 /**
  * A class to keep track of the enrollment state for a given client.
@@ -38,13 +41,21 @@ public abstract class EnrollClient extends ClientMonitor {
     private static final long MS_PER_SEC = 1000;
     private static final int ENROLLMENT_TIMEOUT_MS = 60 * 1000; // 1 minute
     private byte[] mCryptoToken;
+    private boolean mDisplayFODView;
+    private IStatusBarService mStatusBarService;
+    private IVendorFingerprintExtensions mExtDaemon = null;
+    private static final int DISABLE_FP_LONGPRESS = 4;
+    private static final int RESUME_FP_ENROLL = 8;
+    private static final int FINISH_FP_ENROLL = 10;
 
     public EnrollClient(Context context, long halDeviceId, IBinder token,
             IFingerprintServiceReceiver receiver, int userId, int groupId, byte [] cryptoToken,
-            boolean restricted, String owner) {
+            boolean restricted, String owner, IStatusBarService statusBarService) {
         super(context, halDeviceId, token, receiver, userId, groupId, restricted, owner);
         mCryptoToken = Arrays.copyOf(cryptoToken, cryptoToken.length);
         mFacola = new FacolaView(context);
+        mDisplayFODView = context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+        mStatusBarService = statusBarService;
     }
 
     @Override
@@ -72,7 +83,15 @@ public abstract class EnrollClient extends ClientMonitor {
         MetricsLogger.action(getContext(), MetricsEvent.ACTION_FINGERPRINT_ENROLL);
         try {
             receiver.onEnrollResult(getHalDeviceId(), fpId, groupId, remaining);
-            if(remaining == 0) mFacola.hide();
+            if(remaining == 0 && mDisplayFODView) {
+                try {
+                    mExtDaemon = IVendorFingerprintExtensions.getService();
+                    mExtDaemon.updateStatus(FINISH_FP_ENROLL);
+                    mStatusBarService.handleInDisplayFingerprintView(false, true);
+                } catch (RemoteException e) {}
+            } else if (remaining == 0) {
+                mFacola.hide();
+            }
             return remaining == 0;
         } catch (RemoteException e) {
             Slog.w(TAG, "Failed to notify EnrollResult:", e);
@@ -89,7 +108,16 @@ public abstract class EnrollClient extends ClientMonitor {
         }
         Slog.w(TAG, "Starting enroll");
 
-        mFacola.show();
+        if (mDisplayFODView) {
+            try {
+                mExtDaemon = IVendorFingerprintExtensions.getService();
+                mExtDaemon.updateStatus(RESUME_FP_ENROLL);
+                mStatusBarService.handleInDisplayFingerprintView(true, true);
+                mExtDaemon.updateStatus(DISABLE_FP_LONGPRESS);
+            } catch (RemoteException e) {}
+        } else {
+            mFacola.show();
+        }
 
         final int timeout = (int) (ENROLLMENT_TIMEOUT_MS / MS_PER_SEC);
         try {
@@ -112,7 +140,15 @@ public abstract class EnrollClient extends ClientMonitor {
             Slog.w(TAG, "stopEnroll: already cancelled!");
             return 0;
         }
-        mFacola.hide();
+
+        if (mDisplayFODView) {
+            try {
+                mStatusBarService.handleInDisplayFingerprintView(false, true);
+            } catch (RemoteException e) {}
+        } else {
+            mFacola.hide();
+        }
+
         IBiometricsFingerprint daemon = getFingerprintDaemon();
         if (daemon == null) {
             Slog.w(TAG, "stopEnrollment: no fingerprint HAL!");
